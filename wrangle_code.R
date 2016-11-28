17c53cc9b04f3cfbaef0d36637b05a8b0a488b41
zrodnick <zrodnick@gmail.com>
2016-11-28 16:32:43 -0600

    Added a markdown document for the purpose of just taking notes. Wrangled the data a bit.

--- wrangle_code.R
+++ wrangle_code.R
@@ -2,8 +2,11 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 
-sessions_df <- read.csv("sessions.csv")
-train_df_start <- read.csv("train_users_2.csv")
+sessions_df <- read.csv("sessions.csv", header=TRUE, na.strings = c("", " ", "NA"))
+train_df_start <- read.csv("train_users_2.csv", header=TRUE, na.strings = c("", " ", "NA", "-unknown-", "NDF"))
 summarystats_df <- read.csv("age_gender_bkts.csv")
 countries_df <- read.csv("countries.csv")
 
+train_df1 <- left_join(train_df_start, countries_df, by="country_destination")
+train_df1$date_account_created <- as.Date(train_df1$date_account_created)
+train_df1$date_first_booking<- as.Date(train_df1$date_first_booking)
