90f65d988cd6e6ea12034556e7a7edac16166ae2
zrodnick <zrodnick@gmail.com>
2016-12-15 14:04:05 -0600

    More notes.

--- wrangle_code.R
+++ wrangle_code.R
@@ -10,11 +10,17 @@ countries_df <- read.csv("countries.csv")
 train_df1 <- left_join(train_df_start, countries_df, by="country_destination")
 train_df1$date_account_created <- as.Date(train_df1$date_account_created)
 train_df1$date_first_booking<- as.Date(train_df1$date_first_booking)
+train_df1$country_destination <- as.factor(train_df1$country_destination)
+train_df1$booked <- train_df1$country_destination != "NDF"
+train_df1 <- train_df1[, c(1:15,23,16:22)]
 
 sessions_df <- group_by(sessions_df, user_id)
 
 actions <- as.data.frame(levels(sessions_df$action))
 colnames(actions) <- "actions"
 
-train_df1$booked <- train_df1$country_destination != "NDF"
-train_df2 <- train_df1[, c(1:15,23,16:22)]
+
+
+simple_1 <- select(train_df1,gender,booked,country_destination)
+
+
